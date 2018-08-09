c9566c0652fb79c111e46eb622b1985e75082cc0
lehmancurtis147 <lehmancurtis147@gmail.com>
2018-08-08 20:49:02 -0500

    additions

--- Android.mk
+++ Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter codename,$(TARGET_DEVICE)),cp3622a)
-
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+ifneq ($(filter codename,$(TARGET_DEVICE)),cp3622a)
 
 endif
 
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,29 +42,33 @@ BOARD_KERNEL_CMDLINE := \
 	androidboot.console=ttyHSL0 \
 	androidboot.hardware=qcom \
 	user_debug=31 \
-	msm_rtb.filter=0x3F \
+	msm_rtb.filter=0x237 \
 	ehci-hcd.park=3 \
 	androidboot.bootdevice=7824900.sdhci \
 	lpm_levels.sleep_disabled=1 \
 	earlyprintk
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE         := 0x80000000
-BOARD_NAME                := cp3622a
+BOARD_NAME                := msm8909
 BOARD_KERNEL_PAGESIZE     := 2048
 BOARD_HASH_TYPE           := sha1
 BOARD_KERNEL_OFFSET       := 0x00008000
 BOARD_RAMDISK_OFFSET      := 0x01000000
+BOARD_SECOND_OFFSET       := 0x00f00000
 BOARD_KERNEL_TAGS_OFFSET  := 0x00000100
-BOARD_KERNEL_SEPERATED_DT := false
+BOARD_KERNEL_SEPERATED_DT := true
 BOARD_MKBOOTIMG_ARGS := \
 	--base $(BOARD_KERNEL_BASE) \
 	--pagesize $(BOARD_KERNEL_PAGESIZE) \
 	--kernel_offset $(BOARD_KERNEL_OFFSET) \
 	--ramdisk_offset $(BOARD_RAMDISK_OFFSET) \
+	--second_offset $(BOARD_SECOND_OFFSET) \
 	--tags_offset $(BOARD_KERNEL_TAGS_OFFSET) \
 	--cmdline '$(BOARD_KERNEL_CMDLINE)'
 
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/zImage
+TARGET_PREBUILT_DT := $(DEVICE_PATH)/prebuilt/dt
+# BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 
 # TARGET_KERNEL_SOURCE := kernel/coolpad/cp3622a
 # TARGET_KERNEL_CONFIG := msm8909_defconfig
